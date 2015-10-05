1e373698dc01b7d4634b90dd77206c071b1050f3
Tony DiPasquale <tony@thoughtbot.com>
2015-10-05 11:38:34 -0700

    Add install step for Xcode theme

--- install.sh
+++ install.sh
@@ -6,8 +6,9 @@ echo "Interactively linking dotfiles into ~..."
 echo "Installing Vim packages..."
 vim +PlugInstall +qa
 
-echo "Disabling non-sandbox cabal installs..."
-echo "require-sandbox: True" >> ~/.cabal/config
+echo "Installing Xcode theme..."
+mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
+cp xcode/themes/Solarized\ -\ Dark.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
 
 echo "If you like what you see in system/osx-settings, run ./system/osx-settings"
 echo "If you're using Terminal.app, check out the terminal-themes directory"
