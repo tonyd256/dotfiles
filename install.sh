0510817ace4daf25a89cd052009516073f4e3786
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2021-01-15 09:23:05 -0800

    Don't use xcode anymore

--- install.sh
+++ install.sh
@@ -6,9 +6,4 @@ echo "Interactively linking dotfiles into ~..."
 echo "Installing Vim packages..."
 vim +PlugInstall +qa
 
-echo "Installing Xcode theme..."
-mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
-cp xcode/themes/Solarized\ -\ Dark.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
-
 echo "If you like what you see in system/osx-settings, run ./system/osx-settings"
-echo "If you're using Terminal.app, check out the terminal-themes directory"
